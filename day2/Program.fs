80223622a1fea5512cb36beb44cc22fedd493544
Per Rovegård <per@rovegard.se>
2020-12-02 20:15:46 +0100

    Day 2 in F#

---
+++ day2/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open System.Linq
+open fsutils.FsUtils
+
+type Entry = { min: int; max: int; letter: char; password: string }
+
+let parseLine (line:string) : Entry =
+    let parts = line.Split('-', ':', ' ').Where(fun p -> p <> "").ToArray()
+    let min = Int32.Parse parts.[0]
+    let max = Int32.Parse parts.[1]
+    let letter = parts.[2].[0]
+    let password = parts.[3]
+    { min = min; max = max; letter = letter; password = password }
+    
+let passwordMatchesPart1 (e: Entry) =
+    let actual = e.password.Count(fun ch -> ch = e.letter)
+    e.min <= actual && actual <= e.max
+    
+let passwordMatchesPart2 (e: Entry) =
+    let ch1 = e.password.[e.min - 1]
+    let ch2 = e.password.[e.max - 1]
+    xor (ch1 = e.letter) (ch2 = e.letter)
+    
+let part (lines: seq<string>) (matcher: Entry -> bool) =
+    let matchCount = lines.Select(parseLine).Count(fun e -> matcher e)
+    printfn "%d" matchCount
+
+let part1 (lines: seq<string>) = part lines passwordMatchesPart1
+let part2 (lines: seq<string>) = part lines passwordMatchesPart2
+
+[<EntryPoint>]
+let main argv =
+    let lines = readLines "input"
+    part1 lines
+    part2 lines
+    0 // return an integer exit code
---
+++ fsutils/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace fsutils
+
+open System.IO
+
+module FsUtils =
+
+    let readLines (filePath:string) = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
+
+    // No xor in F#??
+    let xor (a: bool) (b: bool) = (a && not b) || (b && not a)
