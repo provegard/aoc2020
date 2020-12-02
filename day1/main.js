5b46c4bceaf097b517b68066a01bddb638af75c6
Per Rovegård <per@rovegard.se>
2020-12-02 22:13:54 +0100

    Tuple version of JS day 1

--- day1/main.js
+++ day1/main.js
@@ -1,45 +1,60 @@
 const fs = require("fs").promises
 
-
-
-
-
-
-
 async function readInput() {
     const contents = await fs.readFile("input", { encoding: "utf-8"})
     return contents.split(/\r?\n/).map(Number)
 }
 
-
-
-async function p1() {
-    const numbers = await readInput()
-    for (let i = 0; i < numbers.length; i++)
-        for (let j = i + 1; j < numbers.length; j++) {
-                const a = numbers[i]
-                const b = numbers[j]
-                if (a + b === 2020) {
-                    console.log(a * b)
-                    return
-                }
-            }
+function sum(tup) {
+    return tup.reduce((s, i) => s + i, 0)
 }
 
-async function p2() {
-    const numbers = await readInput()
-    for (let i = 0; i < numbers.length; i++)
-        for (let j = i + 1; j < numbers.length; j++)
-            for (let z = j + 1; z < numbers.length; z++) {
-                const a = numbers[i]
-                const b = numbers[j]
-                const c = numbers[z]
-                if (a + b + c === 2020) {
-                    console.log(a * b * c)
-                    return
-                }
+function mul(tup) {
+    return tup.reduce((s, i) => s * i, 1)
+}
+
+function first(iter, predicate) {
+    for (const item of iter) {
+        if (predicate(item)) {
+            return item
+        }
+    }
+    throw new Error("No item")
+}
+
+function *genTuples(tup, list, startIdx, tupleLen) {
+    if (tupleLen === 0) throw new Error("tupleLen === 0")
+    else if (tupleLen === 1) {
+        for (let i = startIdx, j = list.length; i < j; i++) yield [...tup, list[i]]
+    }
+    else {
+        for (let i = startIdx, j = list.length; i < j; i++) {
+            const t2 = [...tup, list[i]]
+            for (const t of genTuples(t2, list, i + 1, tupleLen - 1)) {
+                yield t
             }
+
+        }
+    }
 }
 
 
-p1().then(p2)
+function tuples(list, tupleLen) {
+    return genTuples([], list, 0, tupleLen)
+}
+
+async function p12() {
+    const numbers = await readInput()
+    const tups = tuples(numbers, 2)
+    const t = first(tups, tx => sum(tx) === 2020)
+    console.log(mul(t))
+}
+
+async function p22() {
+    const numbers = await readInput()
+    const tups = tuples(numbers, 3)
+    const t = first(tups, tx => sum(tx) === 2020)
+    console.log(mul(t))
+}
+
+p12().then(p22)
