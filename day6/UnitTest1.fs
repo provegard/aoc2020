db4894c3e6d65ca8e96883dab258563cdbf9d8e7
Per Rovegård <per@rovegard.se>
2020-12-06 11:24:17 +0100

    Day 6 part 2

--- day6/UnitTest1.fs
+++ day6/UnitTest1.fs
@@ -12,11 +12,32 @@ let toSet (persons: seq<string>) : Set<char> =
     
 let toSets (lines: seq<string>) : seq<Set<char>> = readBatches lines |> Seq.map toSet
 
+let allQ : Set<char> = Set.ofList [ for ascii in 97 .. 122 -> char ascii ]
+
+let toIntersect (persons: seq<string>) : Set<char> =
+    persons
+        |> Seq.map (fun str -> str.ToCharArray() |> Set.ofArray)
+        |> Seq.reduce (fun s1 s2 -> Set.intersect s1 s2)
+
+let toIntersects (lines: seq<string>) : seq<Set<char>> = readBatches lines |> Seq.map toIntersect
+
 [<Test>]
-let Test1 () =
+let part1 () =
     let result =
         readInput
         |> toSets
         |> Seq.map (fun s -> s.Count)
         |> Seq.fold (+) 0
     Assert.That(result, Is.EqualTo(6416))
+
+
+let part2 (lines: seq<string>) =
+    lines
+        |> toIntersects
+        |> Seq.map (fun s -> s.Count)
+        |> Seq.fold (+) 0
+
+[<Test>]
+let part2Test () =
+    let result = part2 readInput
+    Assert.That(result, Is.EqualTo(3050))
