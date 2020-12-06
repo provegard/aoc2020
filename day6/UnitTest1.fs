d5a765029c7a9bb90d0ec243199d4e325554f160
Per Rovegård <per@rovegard.se>
2020-12-06 11:31:40 +0100

    Day 6 refactoring

--- day6/UnitTest1.fs
+++ day6/UnitTest1.fs
@@ -5,37 +5,30 @@ open fsutils.FsUtils
 
 let readInput = readLines "../../../input"
 
-let toSet (persons: seq<string>) : Set<char> =
-    persons
-        |> Seq.collect (fun str -> str.ToCharArray())
-        |> Seq.fold (fun s ch -> Set.add ch s) Set.empty
-    
-let toSets (lines: seq<string>) : seq<Set<char>> = readBatches lines |> Seq.map toSet
-
-let allQ : Set<char> = Set.ofList [ for ascii in 97 .. 122 -> char ascii ]
-
-let toIntersect (persons: seq<string>) : Set<char> =
+let doCombine (comb: Set<char> -> Set<char> -> Set<char>) (persons: seq<string>) : Set<char> =
     persons
         |> Seq.map (fun str -> str.ToCharArray() |> Set.ofArray)
-        |> Seq.reduce (fun s1 s2 -> Set.intersect s1 s2)
+        |> Seq.reduce (fun s1 s2 -> comb s1 s2)
 
-let toIntersects (lines: seq<string>) : seq<Set<char>> = readBatches lines |> Seq.map toIntersect
+let combine (comb: Set<char> -> Set<char> -> Set<char>) (lines: seq<string>) : seq<Set<char>> =
+    readBatches lines |> Seq.map (doCombine comb)
 
-[<Test>]
-let part1 () =
-    let result =
-        readInput
-        |> toSets
+let part1 (lines: seq<string>) =
+    lines
+        |> combine Set.union
         |> Seq.map (fun s -> s.Count)
         |> Seq.fold (+) 0
-    Assert.That(result, Is.EqualTo(6416))
-
 
 let part2 (lines: seq<string>) =
     lines
-        |> toIntersects
+        |> combine Set.intersect
         |> Seq.map (fun s -> s.Count)
         |> Seq.fold (+) 0
+        
+[<Test>]
+let part1Test () =
+    let result = part1 readInput
+    Assert.That(result, Is.EqualTo(6416))
 
 [<Test>]
 let part2Test () =
