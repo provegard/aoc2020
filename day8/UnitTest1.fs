d2b96607b16b05226b6c89db3ef5b80bf70c73e2
Per Rovegård <per@rovegard.se>
2020-12-08 22:23:00 +0100

    Day 8 cleanup

--- day8/UnitTest1.fs
+++ day8/UnitTest1.fs
@@ -8,30 +8,33 @@ type Instruction = { opcode: string; value: int }
 type Program = { instructions: list<Instruction> }
 type State = { accumulator: int; ip: int; visited: Set<int> }
 
+type Done = { accumulator: int; error: bool }
+
 let readInput = readLines "../../../input"
 
-let step (p: Program) (s: State) : State =
+let step (p: Program) (s: State) : State*Done option =
     let ip = s.ip
     let visited = s.visited
     let accumulator = s.accumulator
     
-    if Set.contains ip visited then
-        raise (InvalidOperationException (sprintf "already visited: %d - with accumulator %d" ip accumulator))
-    
-    let visited' = Set.add ip visited
-    
     if ip >= (List.length p.instructions) then
-        raise (Exception (sprintf "End of program - with accumulator %d" accumulator))        
-    
-    let ins = p.instructions.Item ip
-    let (ip', accumulator') =
-        match ins.opcode with
-        | "nop" -> (ip + 1, accumulator)
-        | "acc" -> (ip + 1, accumulator + ins.value)
-        | "jmp" -> (ip + ins.value, accumulator)
-        | _ -> raise (Exception (sprintf "unknown instruction: %s" ins.opcode))
-    let s' = { accumulator = accumulator'; ip = ip'; visited = visited' }
-    s'
+        // end of program
+        (s, Some({ accumulator = accumulator; error = false }))
+    elif Set.contains ip visited then
+        // error
+        (s, Some({ accumulator = accumulator; error = true }))
+    else
+        let visited' = Set.add ip visited
+        
+        let ins = p.instructions.Item ip
+        let (ip', accumulator') =
+            match ins.opcode with
+            | "nop" -> (ip + 1, accumulator)
+            | "acc" -> (ip + 1, accumulator + ins.value)
+            | "jmp" -> (ip + ins.value, accumulator)
+            | _ -> raise (Exception (sprintf "unknown instruction: %s" ins.opcode))
+        let s' = { accumulator = accumulator'; ip = ip'; visited = visited' }
+        (s', None)
     
 let changeIns (p: Program) (at: int) : Program =
     let ins = p.instructions.Item at
@@ -53,38 +56,39 @@ let lineToInstruction (line: string) : Instruction =
     { opcode = parts.[0]; value = (int parts.[1]) }
 
 let parseProgram (lines: seq<string>) : Program =
-    let instr =
-        lines
-        |> Seq.map lineToInstruction
-        |> List.ofSeq
+    let instr = List.ofSeq (Seq.map lineToInstruction lines)
     { instructions = instr }
     
-let runProgram (p: Program) : Unit =
-    let mutable s = newState
-    while true do
-        s <- step p s
+let runProgram (p: Program) : Done =
+    let rec runRec (p: Program) (s: State) : Done =
+        match step p s with
+        | (_, Some(d)) -> d         // done, either error or success
+        | (s', None) -> runRec p s' // not done, continue running
+    runRec p newState
         
-let bruteForce (p: Program): Unit =
-    let changeIndexes =
-        p.instructions
-        |> List.indexed
-        |> List.where (fun (idx, ins) -> ins.opcode = "nop" || ins.opcode = "jmp")
-        |> List.map fst
-    for idx in changeIndexes do
-        let newP = changeIns p idx
-        try
-            runProgram newP
-        with
-        | :? InvalidOperationException -> ()
+let bruteForce (p: Program): Done =
+    p.instructions
+    |> List.indexed
+    |> List.where (fun (_, ins) -> ins.opcode = "nop" || ins.opcode = "jmp")
+    |> List.map fst
+    |> List.unfold (fun indexes ->
+        match indexes with
+        | i1 :: rest ->
+            match runProgram (changeIns p i1) with
+            | d when d.error -> Some(d, rest) // error, continue generation with the remaining indexes
+            | d -> Some(d, [])                // success, generate this Done value but then stop
+        | [] -> None
+        )
+    |> List.last                              // the last Done value is the successful one
 
 [<Test>]
 let part1 () =
     let p = parseProgram readInput
-    runProgram p // acc = 1709
-    Assert.Pass()
+    let d = runProgram p // acc = 1709
+    Assert.That(d.accumulator, Is.EqualTo(1709))
     
 [<Test>]
 let part2 () =
     let p = parseProgram readInput
-    bruteForce p // acc = 1976
-    Assert.Fail "x"
+    let d = bruteForce p // acc = 1976
+    Assert.That(d.accumulator, Is.EqualTo(1976))
