1afcde62e09b678019899bc5ec0829562e801bec
Per Rovegård <per@rovegard.se>
2020-12-24 14:19:45 +0100

    Day 23 part 2

--- day23/UnitTest2.fs
+++ day23/UnitTest2.fs
@@ -7,9 +7,6 @@ type Cup = { label: int; mutable nextPtr: Cup option; mutable minusOnePtr: Cup o
 
 type Game = { mutable curPtr: Cup; maxPtr: Cup; onePtr: Cup; size: int }
 
-let isDetached (c: Cup) : bool =
-    false
-    
 let rec cupsAfter (c: Cup) (count: int) : list<Cup> =
     if count = 0 then
         []
@@ -43,51 +40,100 @@ let makeMove (g: Game) : Unit =
     
 let toCupLabels (s: string) : list<int> =
     s.ToCharArray() |> Array.map (fun ch -> Int32.Parse (sprintf "%c" ch)) |> List.ofArray
+    
+let rec findCup (f: Cup -> bool) (c: Cup) : Cup =
+    if (f c) then c else (findCup f c.nextPtr.Value)
 
-let buildGame (cupLabels: list<int>) : Game =
-    let size = List.length cupLabels
+let buildGame (cupLabels: list<int>) (maxLabel: int) : Game =
+    let size = maxLabel // assuming 1 .. maxLabel
+    
+    let maxInitialLabel = List.max cupLabels //  initialCups |> List.maxBy (fun c -> c.label)
     let initialCups: list<Cup> = cupLabels |> List.map (fun label -> { label = label; nextPtr = None; minusOnePtr = None })
-    let indexedCups = List.indexed initialCups
     
-    let maxCup = initialCups |> List.maxBy (fun c -> c.label)
+    let mutable oneCup: Cup option = None
     
-    for (idx, cup) in indexedCups do
-        let minusOneLabel = cup.label - 1
-        let nextIdx = (idx + 1) % size
-
-        let minusOneCupOpt = List.tryFind (fun c -> c.label = minusOneLabel) initialCups
-        cup.minusOnePtr <- (Option.orElse (Some(maxCup)) minusOneCupOpt)
-
-        let (_, nextCup) = List.find (fun (i, c) -> i = nextIdx) indexedCups
-        cup.nextPtr <- Some(nextCup)
-    
-    let headCup = List.head initialCups
-    
-    let oneCup = List.find (fun c -> c.label = 1) initialCups
+    let mutable prevCup: Cup option = None
+    let mutable headCup: Cup option = None
+    for i in 1 .. maxLabel do
+        let cup =
+            if i <= maxInitialLabel then
+                List.item (i - 1) initialCups
+            else
+                { label = i; minusOnePtr = None; nextPtr = None }
         
-    { curPtr = headCup; maxPtr = maxCup; onePtr = oneCup; size = size }
+        // Assign headCup the first time
+        if headCup.IsNone then
+            headCup <- Some(cup)
+            
+        // Assign oneCup when we see label 1
+        if cup.label = 1 then
+            oneCup <- Some(cup)
+
+        cup.minusOnePtr <-
+            match prevCup with
+            | Some(c) when c.label = (cup.label - 1) -> Some(c)
+            | _ -> List.tryFind (fun c -> c.label = cup.label - 1) initialCups
+            
+        // Connect the previous cup to this one
+        Option.iter (fun p -> p.nextPtr <- Some(cup)) prevCup
+        
+        prevCup <- Some(cup)
+        
+    // Let the last cup point to the first
+    Option.iter (fun c -> c.nextPtr <- headCup) prevCup
+    
+    let maxCup =
+        match prevCup with
+        | Some(c) when c.label > maxInitialLabel -> c
+        | _ -> findCup (fun c -> c.label = maxInitialLabel) headCup.Value // we'll find it in the first few cups
+
+    // Cup with label 1 has max as prev
+    Option.iter (fun c -> c.minusOnePtr <- Some(maxCup)) oneCup
+        
+    { curPtr = headCup.Value; maxPtr = maxCup; onePtr = oneCup.Value; size = size }
     
 let play (g: Game) (moves: int): string =
     for i in 1 .. moves do
         makeMove g
     let collected = cupsAfter (g.onePtr) (g.size - 1)
     String.concat "" (collected |> List.map (fun c -> sprintf "%d" (c.label)))
-    
+
+let play2 (g: Game) (moves: int): uint64 =
+    for i in 1 .. moves do
+        makeMove g
+    let collected = cupsAfter (g.onePtr) 2
+    let c1 = List.head collected
+    let c2 = List.item 1 collected
+    let l1 = uint64 c1.label
+    let l2 = uint64 c2.label
+    l1 * l2
 
 [<Test>]
 let Test1 () =
-    let game = buildGame (toCupLabels "389125467")
+    let game = buildGame (toCupLabels "389125467") 9
     let res = play game 10
     Assert.That(res, Is.EqualTo("92658374"))
     
 [<Test>]
 let Test1b () =
-    let cups = buildGame (toCupLabels "389125467")
+    let cups = buildGame (toCupLabels "389125467") 9
     let res = play cups 100
     Assert.That(res, Is.EqualTo("67384529"))
     
 [<Test>]
 let Part1 () =
-    let cups = buildGame (toCupLabels "156794823")
+    let cups = buildGame (toCupLabels "156794823") 9
     let res = play cups 100
     Assert.That(res, Is.EqualTo("82573496"))
+    
+[<Test>]
+let Test2 () =
+    let game = buildGame (toCupLabels "389125467") 1_000_000
+    let res = play2 game 10000000
+    Assert.That(res, Is.EqualTo(149245887792UL))
+    
+[<Test>]
+let Part2 () =
+    let game = buildGame (toCupLabels "156794823") 1_000_000
+    let res = play2 game 10000000
+    Assert.That(res, Is.EqualTo(11498506800UL))
